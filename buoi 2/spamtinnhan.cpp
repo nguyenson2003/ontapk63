baec91368fcc452ed22e857d55a2d60ac0e947c8
nguyenson2003 <nqson123@gmail.com>
2022-11-10 20:20:17 +0700

    Update spamtinnhan.cpp

--- buoi 2/spamtinnhan.cpp
+++ buoi 2/spamtinnhan.cpp
@@ -14,21 +14,29 @@ int get(int line){
     if(line <= k) return csc(line);
     return csc(k)+csc(k-1)-csc(k*2-1-line);
 }
-int search(int l,int r){
 
+int search(int l,int r){
+    if(l==r) return l;
+    int mid=(l+r)/2;
+    int temp = get(mid);
+    if(temp<x) return search(mid+1,r);
+    return search(l,mid);
 }
 keqing(){ 
     int t; cin >> t;
     while(t--){//0: false, else: true
         cin>>k>>x;
         //while
+        /*
         int l=1,r=k*2-1;
         while(l<r){
             int mid=(l+r)/2;
-            int temp = get(mid,k);
+            int temp = get(mid);
             if(temp<x) l=mid+1;
             else r=mid;
         }// l==r
-        cout<<l<<endl;
+        */
+       //search đệ quy
+        cout<< search(1,k*2-1)<<endl;
     }
 }
