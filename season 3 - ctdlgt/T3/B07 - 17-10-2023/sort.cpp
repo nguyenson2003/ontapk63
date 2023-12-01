18da5b80e31c6bf554b291a70b399da0f0541603
nguyenson2003 <59283327+nguyenson2003@users.noreply.github.com>
2023-12-01 22:36:57 +0700

    sort

--- season 3 - ctdlgt/T3/B07 - 17-10-2023/sort.cpp
+++ season 3 - ctdlgt/T3/B07 - 17-10-2023/sort.cpp
@@ -33,8 +33,8 @@ insertedSort(int n,int a[]){
 }
 
 
-mergeSort(int a[],int from,int to){
-	if(to-from==0) return;
+void mergeSort(int a[],int from,int to){
+	if(from>=to) return;
 	int mid = (from+to)/2; //chia doi
 	mergeSort(a,from,mid); //sap xep phan ben trai
 	mergeSort(a,mid+1,to); //sap xep phan ben phai
@@ -42,7 +42,7 @@ mergeSort(int a[],int from,int to){
 	int b[to-from+1];
 	
 	int i = from, j = mid+1,k=0;
-	for(;i<=mid && j<=to;k++){
+	for(;i<=mid || j<=to;k++){
 		if(i>mid){
 			b[k]=a[j];
 			j++;
@@ -62,15 +62,37 @@ mergeSort(int a[],int from,int to){
 	for(;i<=to ;i++,k++){
 		a[i]=b[k];
 	}
+	
 }
 
+void quickSort(int a[],int from,int to){
+	if(from>=to)return;
+	
+	
+	int i = from;
+	int j = to-1;
+	while(i<j){
+		while(a[i]<a[to])i++;
+		while(a[j]>a[to])j--;
+		if(i<j)
+			swap(a[i],a[j]);
+	}
+	if(a[i]>a[to])
+		swap(a[i],a[to]);
+		
+		
+	quickSort(a,from,i-1);
+	quickSort(a,i+1,to);
+}
 
 main(){
-	int n = 5;
-	int a[]  = {5,3,1,8,2};
-	mergeSort(n,a);
+	int n = 8;
+	int a[]  = {10,3,24,1,4,21,54,5};
+	quickSort(a,0,n-1);
 	for(int x:a)cout<<x<<' ';
 }
+
+//3 1 5 8 2
 
 
 
